5121bd024cbe97afc8b6499978c02de1a35acb08
su yung cheng <410650930@gms.tku.edu.tw>
2023-11-01 14:25:24 +0800

    W06-P3: Show responsive using minmax approach

---
+++ demo/script_410650930.js
@@ -0,0 +1,33 @@
+function showdemo(week) {
+
+    const p = document.querySelector('.show-classdemo');
+    console.log('show-classdemo', p);
+    switch(week) {
+        case 1:
+            p.innerHTML = `<iframe src='./w01_410650930_introduction' width="100% height="100% />`;
+
+        break;
+
+        case 3:
+            p.innerHTML = `<iframe src='./w03_410650930_card_demo' width="100% height="100% />`;
+
+        break;
+
+        case 4:
+            p.innerHTML = `<iframe src='./w04_410650930_card_demo' width="100% height="100% />`;
+
+        break;
+
+        case 6:
+            p.innerHTML = `<iframe src='./w06_410650930_blog' width="100% height="100% />`;
+
+        break;
+
+        
+
+    }
+
+    
+
+
+}
