90d69473bf4edcadfc6484fa9b00afd1e5fb2bea
su yung cheng <410650930@gms.tku.edu.tw>
2023-11-02 14:18:13 +0800

    W05-P1: Uss css grid to show 2 photos in a row

--- demo/script_410650930.js
+++ demo/script_410650930.js
@@ -4,22 +4,22 @@ function showdemo(week) {
     console.log('show-classdemo', p);
     switch(week) {
         case 1:
-            p.innerHTML = `<iframe src='./w01_410650930_introduction' width="100% height="100% />`;
-
+            p.innerHTML = `<iframe src='./w01_410650930_introduction/' width="100% height="100% />`;
+            
         break;
 
         case 3:
-            p.innerHTML = `<iframe src='./w03_410650930_card_demo' width="100% height="100% />`;
+            p.innerHTML = `<iframe src='./w03_410650930_card_demo/' width="100% height="100% />`;
 
         break;
 
         case 4:
-            p.innerHTML = `<iframe src='./w04_410650930_card_demo' width="100% height="100% />`;
+            p.innerHTML = `<iframe src='./w04_410650930_card_demo/' width="100% height="100% />`;
 
         break;
 
         case 6:
-            p.innerHTML = `<iframe src='./w06_410650930_blog' width="100% height="100% />`;
+            p.innerHTML = `<iframe src='./w06_410650930_blog/' width="100% height="100% />`;
 
         break;
 
