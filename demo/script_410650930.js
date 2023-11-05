3652293799ede493d298de77cec57b6d67234620
su yung cheng <410650930@gms.tku.edu.tw>
2023-11-05 14:40:03 +0800

    W07-P1: Create nav and classdemo with css

--- demo/script_410650930.js
+++ demo/script_410650930.js
@@ -4,22 +4,27 @@ function showdemo(week) {
     console.log('show-classdemo', p);
     switch(week) {
         case 1:
-            p.innerHTML = `<iframe src='./w01_410650930_introduction/' width="100% height="100% />`;
+            p.innerHTML = `<iframe src='./w01_410650930_introduction/' width="100%" height="100%" />`;
             
         break;
 
         case 3:
-            p.innerHTML = `<iframe src='./w03_410650930_card_demo/' width="100% height="100% />`;
+            p.innerHTML = `<iframe src='./w03_410650930_card_demo/' width="100%" height="100%" />`;
 
         break;
 
         case 4:
-            p.innerHTML = `<iframe src='./w04_410650930_card_demo/' width="100% height="100% />`;
+            p.innerHTML = `<iframe src='./w04_410650930_card_demo/' width="100%" height="100%" />`;
 
         break;
 
         case 6:
-            p.innerHTML = `<iframe src='./w06_410650930_blog/' width="100% height="100% />`;
+            p.innerHTML = `<iframe src='./w06_410650930_blog/' width="100%" height="100%" />`;
+
+        break;
+
+        case 7:
+            p.innerHTML = `<iframe src='./w07_410650930/' width="100%" height="100%" />`;
 
         break;
 
